9ea72e183e56c0e4fcb62234640b60460a333ea9
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2026-08-12 10:59:34 -0700

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+# Install uv
+git clone https://github.com/clara-parabricks-workflows/single-cell-analysis-blueprint.git
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+export PATH="$HOME/.local/bin:$PATH"
+uv --version
+
+# Install all additional libraries
+cd /notebooks/single-cell-analysis-blueprint/notebooks
+mamba install -c conda-forge -c rapidsai compilers -y
+uv pip install --system -r requirements.txt
+
+set -m
+
+# Set missing env vars, start the primary process, and put it in the background
+export CONDA_PREFIX=/opt/conda
+jupyter-lab --notebook-dir=/notebooks/single-cell-analysis-blueprint/notebooks --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.allow_origin='*' --allow-root
