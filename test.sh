91d6428ac004d2565ba95ce2dd48f65819e8ba01
Taurean Dyer <46935140+taureandyernv@users.noreply.github.com>
2026-08-12 11:24:39 -0700

    fixed directory commands

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ export PATH="$HOME/.local/bin:$PATH"
 uv --version
 
 # Install all additional libraries
-cd /notebooks/single-cell-analysis-blueprint/notebooks
+cd /notebooks/single-cell-analysis-blueprint
 mamba install -c conda-forge -c rapidsai compilers -y
 uv pip install --system -r requirements.txt
 
@@ -16,4 +16,4 @@ set -m
 
 # Set missing env vars, start the primary process, and put it in the background
 export CONDA_PREFIX=/opt/conda
-jupyter-lab --notebook-dir=/notebooks/single-cell-analysis-blueprint/notebooks --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.allow_origin='*' --allow-root
+jupyter-lab --notebook-dir=/notebooks/single-cell-analysis-blueprint --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.allow_origin='*' --allow-root
